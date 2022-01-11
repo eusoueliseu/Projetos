7fadc69cbbc849fa78cbee1d9abd12cf94db6bb8
Eliseu Santos <86926255+eusoueliseu@users.noreply.github.com>
2022-01-11 04:04:59 -0300

    Update Calculadora Python.py
    Correção da divisão por zero e seleção de operação fora das opções

--- Calculadora Python.py
+++ Calculadora Python.py
@@ -3,7 +3,6 @@
 
 # In[48]:
 
-
 print('''#### Calculadora Python ####
 
 Digite a operação desejada
@@ -19,17 +18,18 @@ conta = 0
 
 operacao = int(input('Operação desejada: '))
 
-if operacao in valores:
-    if operacao == 1:
-        conta = 'Operação SOMA selecionada'
-    elif operacao == 2:
-        conta = 'Operacação SUBTRAÇÃO selecionada'
-    elif operacao == 3:
-        conta = 'Operação MULTIPLICAÇÃO selecionada'
-    elif operacao == 4:
-        conta = 'Operação DIVISÃO selecionada'
-else:
-    print ('Operação inválida')
+if operacao > 4:
+    conta = 'Operação inexistente'
+elif operacao == 1:
+    conta = 'Operação SOMA selecionada'
+elif operacao == 2:
+    conta = 'Operacação SUBTRAÇÃO selecionada'
+elif operacao == 3:
+    conta = 'Operação MULTIPLICAÇÃO selecionada'
+elif operacao == 4:
+    conta = 'Operação DIVISÃO selecionada'
+
+    
     
 print (conta)
 valor1 = int(input('Digite o primeiro valor: '))
@@ -38,7 +38,11 @@ valor2 = int(input('Digite o segundo valor: '))
 soma = valor1 + valor2
 subtracao = valor1 - valor2
 multiplicacao = valor1 * valor2
-divisao = valor1 / valor2
+
+try:
+    divisao = valor1 / valor2
+except ZeroDivisionError:
+    print('Impossível dividir por 0')
 
 if operacao == 1:
     resultado = soma
@@ -49,5 +53,12 @@ elif operacao == 3:
 else:
     resultado = divisao
 
-print ('Resultado: ', resultado)
+resultado = str(resultado)
+    
+a = ['Resultado: ', resultado]
+b = str('')
 
+if (operacao == 4) and (valor2 == 0):
+    print (b)
+else:
+    print (a)
